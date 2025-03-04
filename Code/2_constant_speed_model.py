a64b70fe0850df85c59759f70b37dea4e72099c6
XinyaBi <XinyaBi@outlook.com>
2025-03-04 14:39:56 +0100

    updated constant speed model

--- Code/2_constant_speed_model.py
+++ Code/2_constant_speed_model.py
@@ -1,5 +1,34 @@
 import numpy as np
-from gradient_descent import gradient_descent
+
+# for constant speed model, the formula to fit the points would be pos = initial_pos + speed * time
+# for acceleration speed model, the formula to fit would be pos = initial_pos + speed * time + acceleration * time ^2
+def gradient_descent(start, gradient_func, learning_rate, max_iterations, threshold):
+    """
+       Implementation of gradient descent algorithm
+
+       Parameters:
+       start: Initial guess for parameters, constant speed model would be [0,1], acceleration model would be [0,1,1]
+       gradient_func: Function that returns gradient at current parameters
+       learning_rate: Step size for parameter updates
+       max_iterations: Maximum number of iterations
+       threshold: Stops when step size is below this value
+
+       Returns:
+       params: Optimized parameters
+       """
+    params = np.array(start)
+    for i in range(max_iterations):
+        # calculate the step size
+        step_size = learning_rate * gradient_func(params)
+        # if step size is too small, quit the function
+        if np.all(np.abs(step_size) < threshold):
+            print(f"stop after {i} iterations, current parameters: {params}")
+            break
+        # calculate the new params
+        params = params - step_size
+    print(f"Optimized parameters: {params}")
+    return params
+
 
 def constant_speed_gradient(params, timestamps, pos):
     """
